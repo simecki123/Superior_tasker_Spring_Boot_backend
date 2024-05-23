1660628c30a3c17a00dc3ef4e179afc0f8f361d3
Sime Roncevic <sroncevic19@gmail.com>
2024-05-23 14:07:31 +0200

    adding springboot application from spring initializer to github

---
+++ Superior_tasker_backend_springboot/src/main/java/com/Superior_tasker_backend/Superior_tasker_backend_springboot/SuperiorTaskerBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.Superior_tasker_backend.Superior_tasker_backend_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperiorTaskerBackendSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperiorTaskerBackendSpringbootApplication.class, args);
+	}
+
+}
---
+++ Superior_tasker_backend_springboot/src/test/java/com/Superior_tasker_backend/Superior_tasker_backend_springboot/SuperiorTaskerBackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Superior_tasker_backend.Superior_tasker_backend_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperiorTaskerBackendSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
