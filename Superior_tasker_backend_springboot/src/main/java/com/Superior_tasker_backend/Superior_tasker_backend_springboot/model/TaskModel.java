4a3c638578e59e4b0d7466bd475b81f2d362e76f
Sime Roncevic <sroncevic19@gmail.com>
2024-05-23 15:40:46 +0200

    Creating new model, object for Tasks

---
+++ Superior_tasker_backend_springboot/src/main/java/com/Superior_tasker_backend/Superior_tasker_backend_springboot/model/TaskModel.java
@@ -0,0 +1,46 @@
+package com.Superior_tasker_backend.Superior_tasker_backend_springboot.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "UserCollection")
+public class TaskModel {
+
+    @Id
+    private String id;
+    private String ProjectId;
+    private String name;
+    private Boolean done;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProjectId() {
+        return ProjectId;
+    }
+
+    public void setProjectId(String projectId) {
+        ProjectId = projectId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+}
