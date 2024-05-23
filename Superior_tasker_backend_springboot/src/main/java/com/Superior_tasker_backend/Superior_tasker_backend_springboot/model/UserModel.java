5c3ea73267f88e6a3a247b811027162fd1b0310e
Sime Roncevic <sroncevic19@gmail.com>
2024-05-23 15:30:42 +0200

    New model User object is created

---
+++ Superior_tasker_backend_springboot/src/main/java/com/Superior_tasker_backend/Superior_tasker_backend_springboot/model/UserModel.java
@@ -0,0 +1,74 @@
+package com.Superior_tasker_backend.Superior_tasker_backend_springboot.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "UserCollection")
+public class UserModel {
+
+    @Id
+    private String Id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String description;
+    private String image;
+
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
