5d5a51a6772817b983ba2cc0f315eeb60d0a9c1c
Sime Roncevic <sroncevic19@gmail.com>
2024-05-26 10:54:25 +0200

    Adding new CRUD function for projects and tasks

--- Superior_tasker_backend_springboot/src/main/java/com/Superior_tasker_backend/Superior_tasker_backend_springboot/repository/ProjectRepository.java
+++ Superior_tasker_backend_springboot/src/main/java/com/Superior_tasker_backend/Superior_tasker_backend_springboot/repository/ProjectRepository.java
@@ -3,5 +3,8 @@ package com.Superior_tasker_backend.Superior_tasker_backend_springboot.repositor
 import com.Superior_tasker_backend.Superior_tasker_backend_springboot.model.ProjectModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ProjectRepository extends MongoRepository<ProjectModel, String> {
+    List<ProjectModel> findByUserId(String userId);
 }
--- Superior_tasker_backend_springboot/src/main/java/com/Superior_tasker_backend/Superior_tasker_backend_springboot/repository/TaskRepository.java
+++ Superior_tasker_backend_springboot/src/main/java/com/Superior_tasker_backend/Superior_tasker_backend_springboot/repository/TaskRepository.java
@@ -3,5 +3,8 @@ package com.Superior_tasker_backend.Superior_tasker_backend_springboot.repositor
 import com.Superior_tasker_backend.Superior_tasker_backend_springboot.model.TaskModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends MongoRepository<TaskModel, String> {
+    List<TaskModel> findByProjectId(String id);
 }
