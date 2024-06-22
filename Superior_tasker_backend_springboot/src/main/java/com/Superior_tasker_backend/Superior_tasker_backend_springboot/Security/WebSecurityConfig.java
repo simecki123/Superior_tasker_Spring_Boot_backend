4095f50818f3f7f57e442f9a7c9efe38f4647ae8
Sime Roncevic <sroncevic19@gmail.com>
2024-06-22 15:16:17 +0200

    Adding functionality to adress token that are sent from frontend as valid

--- Superior_tasker_backend_springboot/src/main/java/com/Superior_tasker_backend/Superior_tasker_backend_springboot/Security/WebSecurityConfig.java
+++ Superior_tasker_backend_springboot/src/main/java/com/Superior_tasker_backend/Superior_tasker_backend_springboot/Security/WebSecurityConfig.java
@@ -15,6 +15,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -34,9 +40,9 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/api/login","/tasks/updateTask/{id}", "/tasks/findAllTasksOfUser/{userId}", "/projects/saveProject","/projects/getProjectById/{id}", "/tasks/saveTask", "/user/updateUser/{id}", "/projects/deleteProject/{id}", "/tasks/delete/{projectId}", "/tasks/findAllTasksOfProject/{projectId}", "/projects/findAllProjects/{userId}", "/api/register").permitAll()
+                        .requestMatchers("/api/login", "/api/register").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
@@ -45,6 +51,18 @@ public class WebSecurityConfig {
                 .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
